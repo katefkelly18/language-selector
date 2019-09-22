285c1ec231ade76c6c667d822d18577a21061755
Kate Kelly <katefkelly18@gmail.com>
2019-09-22 10:10:59 -0700

    Continue efforts on jquery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 $(document).ready(function() {
-  
+  $("#language-selector").submit(function(event) {
+    var education = $("input:radio[name=education]:checked").val();
+    var level = $("input:radio[name=level]:checked").val();
+    var work = $("input:radio[name=work]:checked").val();
+    var time = $("input:radio[name=time]:checked").val();
+    var location = $("input:radio[name=location]:checked").val();
+    $("#results").show();
+    if (education === "highschool" && level === "beginner" && work === "alone") {
+      $("#javascript").show();
+
+    }
+    event.preventDefault();
+  });
+});
