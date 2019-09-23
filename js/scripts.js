251c495d88dfd8df735f8bd316e17eed07af1108
Kate Kelly <katefkelly18@gmail.com>
2019-09-22 19:04:34 -0700

    Continue efforts on debugging Jquery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-  $("#language-selector").submit(function(event) {
+  $("form#language-selector").submit(function(event) {
     var education = $("input:radio[name=education]:checked").val();
     var level = $("input:radio[name=level]:checked").val();
     var work = $("input:radio[name=work]:checked").val();
     var time = $("input:radio[name=time]:checked").val();
     var location = $("input:radio[name=location]:checked").val();
-    $("#results").show();
-    if (education === "highschool" && level === "beginner" && work === "alone") {
+  $("#results").show();
+    if (education === "highschool" && level === "beginner" && work === "alone" && time === "small" && location === "seattle") {
       $("#javascript").show();
-
     }
     event.preventDefault();
   });
